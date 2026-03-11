d95335401befab3fe4cde806b82d8b82b37ff36c
Anurag Kadam <kadamanurag819@gmail.com>
2026-03-11 16:20:00 +0530

    added backend link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://syncro-backend-gamma.vercel.app/api",
 });
 
 // 1. REQUEST Interceptor: Attach token before sending the request
