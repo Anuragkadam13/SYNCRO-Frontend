d7a44fdfad2da9c47f4ce1538b17fa9af0317647
Anurag Kadam <kadamanurag819@gmail.com>
2026-03-12 13:26:59 +0530

    gap decreased

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TaskListNumbers = ({ data }) => {
   return (
-    <div className="flex justify-between gap-4 md:gap-4 flex-col md:flex-row ">
+    <div className="flex justify-between gap-2 md:gap-4 flex-col md:flex-row ">
       <div className="flex rounded-lg w-full py-2 px-4 md:py-5 md:px-10 bg-blue-500 ">
         <h2 className="md:text-md lg:text-xl xl:text-2xl font-semibold">
           {data.taskCounts.newTask} New Task
